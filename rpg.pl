cbf7347fb6509ab1d171ac74f62c69da55aeeb67
Ryan Fox <foxryan@gmail.com>
2010-02-09 22:04:40 -0500

    Added event code to detect if the window was closed, and if so, exit.

--- rpg.pl
+++ rpg.pl
@@ -1,14 +1,17 @@
 #!/usr/bin/perl
 use SDL;
 use SDL::App;
+use SDL::Event;
 use SDL::Surface;
 use SDL::Rect;
 use SDL::Color;
 
 my $app = new SDL::App(-title=>"RPG Aquarium", -width=>640, -height=>480, -depth=>16);
+my $event = new SDL::Event;
 
 my $hero_clr = new SDL::Color(-r=>0,-g=>0,-b=>255);
 my $monster_clr = new SDL::Color(-r=>255,-g=>0,-b=>0);
+my $big_monster_clr = new SDL::Color(-r=>255,-g=>255,-b=>0);
 my $black = new SDL::Color(-r=>0,-g=>0,-b=>0);
 
 use Person;
@@ -16,13 +19,14 @@ use Monster;
 use Object;
 use Room;
 
-use Goals::FindKill;
+use Goals::Gather;
+use Goals::Persecute;
 use Goals::Nothing;
 
 my $room = new Room;
 
 my @heroes;
-for(1..15)
+for(1..30)
 {
     my $x;
     my $y;
@@ -35,12 +39,12 @@ for(1..15)
     
     my $hero = new Person(name => 'Hero'.$_, x => $x, y => $y, max_hp => 100, attack_range => 2, gfx_color=>$hero_clr);
     $room->add_content($hero);
-    $hero->add_goal(new Goals::FindKill(to_find=>'BadGuy'));
+    $hero->add_goal(new Goals::Gather(to_find=>'BadGuy'));
     push @heroes, $hero;
 }
 
 my @monsters;
-for(1..50)
+for(1..100)
 {
     my $x;
     my $y;
@@ -53,11 +57,13 @@ for(1..50)
 
     my $m = new Monster(name => 'Monster'.$_, x => $x, y => $y, max_hp => 40, gfx_color=>$monster_clr);
     $m->add_goal(new Goals::Nothing);
-    $m->add_goal(new Goals::FindKill(to_find=>'GoodGuy'));
+#    $m->add_goal(new Goals::Persecute(to_find=>'GoodGuy'));
     $room->add_content($m);
 }
 
 my $app_rect = new SDL::Rect(-width=>640,-height=>480,-x=>0,-y=>0);
+my $ticks = $app->ticks();
+my $old_ticks = 0;
 
 while(grep{$_->hp > 0 && @{$_->goal_stack}} @heroes)
 {
@@ -71,6 +77,29 @@ while(grep{$_->hp > 0 && @{$_->goal_stack}} @heroes)
         }
     }
     $app->update($app_rect);
-    
-    $app->delay(50);
+
+    $event->pump();
+    $event->poll();
+    exit if $event->type == SDL_QUIT;
+
+    $old_ticks = $ticks;
+    $ticks = $app->ticks;
+    print "Ticks: ".($ticks - $old_ticks)."\n";
+    if ($ticks-$old_ticks < 50)
+    {
+        $app->delay(50 - ($ticks - $old_ticks));
+    }
+}
+
+foreach(@heroes)
+{
+    print $_->name;
+    if ($_->hp > 0)
+    {
+        print " -> HP: ".$_->hp." Level: ".$_->level."\n";
+    }
+    else
+    {
+        print " -> Dead\n";
+    }
 }
