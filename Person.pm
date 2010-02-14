f57b3521cc3946ec3436403e43b0c86371d2f26c
Ryan Fox <foxryan@gmail.com>
2010-02-14 00:03:20 -0500

    People have inventory now.

--- Person.pm
+++ Person.pm
@@ -2,7 +2,7 @@ package Person;
 use Moose;
 
 with 'Living', 'Moves', 'ExperienceLevel', 'Fighter', 'GoodGuy',
-    'GoalOriented', 'Drawable';
+    'GoalOriented', 'HasInventory', 'Drawable';
 
  __PACKAGE__->meta->make_immutable;
 1;
