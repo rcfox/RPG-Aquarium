b2b2c6501fb7a725e2ee609a4e4c35266f4b39fb
Ryan Fox <foxryan@gmail.com>
2010-02-13 23:58:00 -0500

    Things that explode on death will actually explode now.

--- ExplodesOnDeath.pm
+++ ExplodesOnDeath.pm
@@ -5,7 +5,14 @@ with 'Living';
 
 after 'died' => sub
 {
+    my $self = shift;
+    
     print "BOOM!\n";
+    foreach(grep { $self->distance($_) <= $self->max_hp/10 } @{$self->container->all_contents})
+    {
+        print $_->name." is damaged by the explosion!\n";
+        $_->injure($self->max_hp/20);
+    }
 };
 
 1;
