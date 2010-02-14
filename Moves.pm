f8ced254ce5cb98e7dbff76479e4fa6699fe39ce
Ryan Fox <foxryan@gmail.com>
2010-02-14 00:01:22 -0500

    Replaced the lame pathfinding with A*.

--- Moves.pm
+++ Moves.pm
@@ -10,32 +10,45 @@ sub move
     my $dy = shift;
     if ($self->place($self->x + $dx,$self->y + $dy))
     {        
-        print $self->name." moves to (".$self->x.",".$self->y.")\n";
+#        print $self->name." moves to (".$self->x.",".$self->y.")\n";
         return 1;
     }
 
     return 0;
 }
 
-# Lame pathfinding.
 sub pathfind
 {
     my $self = shift;
-    my $x = shift() - $self->x;
-    my $y = shift() - $self->y;
+    my $x = shift;
+    my $y = shift;
     my $moved = 0;
+
+    # Set current and target position passability to 1 so that the pathfinding works.
+    $self->container->map->set_passability($self->x,$self->y,1);
+    $self->container->map->set_passability($x,$y,1);
     
-    if (abs($x) > 0)
+    my $path = $self->container->map->astar($self->x,$self->y,$x,$y);
+
+    # Reset passability
+    $self->container->map->set_passability($self->x,$self->y,0);
+    $self->container->map->set_passability($x,$y,0);
+    
+    my $dir = substr($path,0,1);
+
+    if ($dir)
     {
-        $moved = $self->move($x/abs($x),0);
-    }
-    if (abs($y) > 0)
-    {
-        $moved = $self->move(0,$y/abs($y));
+        $moved = $self->move(-1,1)  if ($dir == 1);
+        $moved = $self->move(0,1)   if ($dir == 2);
+        $moved = $self->move(1,1)   if ($dir == 3);
+        $moved = $self->move(-1,0)  if ($dir == 4);
+        $moved = $self->move(1,0)   if ($dir == 6);
+        $moved = $self->move(-1,-1) if ($dir == 7);
+        $moved = $self->move(0,-1)  if ($dir == 8);
+        $moved = $self->move(1,-1)  if ($dir == 9);
     }
 
     return $moved;
 }
-
 
 1;
