7988067ac0d91405b7af8db00c63721f6c4e62ea
Ryan Fox <foxryan@gmail.com>
2010-02-14 00:06:56 -0500

    Containers hold a map used for the A*. This should have gone with the commit for Moves.pm...

--- Container.pm
+++ Container.pm
@@ -1,6 +1,8 @@
 package Container;
 use Moose::Role;
 
+use AI::Pathfinding::AStar::Rectangle;
+
 has 'contents' =>
     (
         is => 'rw',
@@ -8,12 +10,35 @@ has 'contents' =>
         default => sub{{}},
     );
 
+has 'map' =>
+    (
+        is => 'rw',
+        isa => 'Any',
+        default => sub
+        {
+            AI::Pathfinding::AStar::Rectangle->new({width=>160, height=>120});            
+        },
+    );
+
+sub BUILD
+{
+    my $self = shift;
+    for(my $p = 1; $p < 159; ++$p)
+    {
+        for(my $q = 1; $q < 119; ++$q)
+        {
+            $self->map->set_passability($p,$q,1);
+        }
+    }
+}
+
 sub add_content
 {
     my $self = shift;
     my $toadd = shift;
 
     $self->contents->{$toadd->x}->{$toadd->y} = $toadd;
+    $self->map->set_passability($toadd->x,$toadd->y,0);
 
     $toadd->container($self);
 }
@@ -23,6 +48,7 @@ sub remove_content
     my $self = shift;
     my $toadd = shift;
 
+    $self->map->set_passability($toadd->x,$toadd->y,1);
     delete($self->contents->{$toadd->x}->{$toadd->y});
 }
 
@@ -44,6 +70,15 @@ sub all_contents
     return \@c;
 }
 
+sub contains_object
+{
+    my $self = shift;
+    my $object = shift;
+    my @arr = grep { $_ == $object } @{$self->all_contents};
+
+    return scalar @arr;
+}
+
 sub place_content
 {
     my $self = shift;
@@ -51,8 +86,10 @@ sub place_content
     my $nx = shift;
     my $ny = shift;
 
+    $self->map->set_passability($content->x,$content->y,1);
     delete($self->contents->{$content->x}->{$content->y});
     $self->contents->{$nx}->{$ny} = $content;
+    $self->map->set_passability($nx,$ny,0);
 }
 
 sub check_collision
@@ -61,8 +98,10 @@ sub check_collision
     my $x = shift;
     my $y = shift;
 
+    return 1 if ($x < 0 || $x > 159 || $y < 0 || $y > 119);
+
     my $val = $self->contents->{$x}->{$y};
-    return (defined($val));
+    return ($val ? 1 : 0);
 }
 
 1;
