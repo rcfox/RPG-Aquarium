4284707b3596053bf9d7497b90bd38106cf13eef
Ryan Fox <foxryan@gmail.com>
2010-03-13 02:30:26 -0500

    ExplodesOnDeath only attempts to injure living things now.

--- ExplodesOnDeath.pm
+++ ExplodesOnDeath.pm
@@ -8,7 +8,7 @@ after 'died' => sub
     my $self = shift;
     
     print "BOOM!\n";
-    foreach(grep { $self->distance($_) <= $self->max_hp/10 } @{$self->container->all_contents})
+    foreach(grep { $_->does('Living') && $self->distance($_) <= $self->max_hp/10 } @{$self->container->all_contents})
     {
         print $_->name." is damaged by the explosion!\n";
         $_->injure($self->max_hp/20);
